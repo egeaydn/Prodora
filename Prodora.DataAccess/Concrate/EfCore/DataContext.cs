3cfa3c8e1b06eb136ff89f32a39684d09b23629e
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2025-03-15 22:12:26 +0300

    add to DataContext

---
+++ Prodora.DataAccess/Concrate/EfCore/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Prodora.Entitys;
+
+namespace Prodora.DataAccess.Concrate.EfCore
+{
+    class DataContext : DbContext
+    {
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Server=DESKTOP-L027AII\SQLEXPRESS;Database=Prodora;uid=sa;pwd=1;TrustServerCertificate=True");
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<ProductCategory>().HasKey(p => new { p.ProductId, p.CategoryId });
+		}
+
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<ProductCategory> ProductCategories { get; set; }
+		public DbSet<Image> Images { get; set; }
+
+
+	}
+}
