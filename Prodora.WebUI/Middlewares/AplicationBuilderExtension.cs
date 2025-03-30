cf2267552c663aa046d055ee9bf03059e9a44d92
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2025-03-30 20:28:09 +0300

    ApplicationBuilderExtensions added inside Middlewares file

---
+++ Prodora.WebUI/Middlewares/AplicationBuilderExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.FileProviders;
+
+namespace Prodora.WebUI.Middlewares
+{
+	public static class AplicationBuilderExtension
+	{
+		public static IApplicationBuilder CustomStaticFiles(this IApplicationBuilder app)
+		{
+			var path = Path.Combine(Directory.GetCurrentDirectory(), "node_modules");
+
+			var options = new StaticFileOptions
+
+			{
+				FileProvider = new PhysicalFileProvider(path),
+				RequestPath = "/node_modules",
+			};
+
+			app.UseStaticFiles(options);
+			return app;
+		} 
+	}
+}
