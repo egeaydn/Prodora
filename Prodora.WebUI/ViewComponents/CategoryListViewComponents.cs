d821cbcfdeea82dcf4c6709f4a6e9bf44918b7e4
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2025-04-01 17:53:35 +0300

    CategoryListViewComponents page inside ViewComponents file added

---
+++ Prodora.WebUI/ViewComponents/CategoryListViewComponents.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prodora.Business.Abstract;
+using Prodora.WebUI.Models;
+
+namespace Prodora.WebUI.ViewComponents
+{
+	public class CategoryListViewComponents : ViewComponent
+	{
+		private ICategoryServices _categoryServices;
+		public CategoryListViewComponents(ICategoryServices categoryServices)
+		{
+			_categoryServices = categoryServices;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			return View(
+			new CategoryListViewModel()
+			{
+				Categories = _categoryServices.GetAll(),
+				SelectedCategory = RouteData.Values["category"]?.ToString()
+			}
+			);
+		}
+	}
+}
