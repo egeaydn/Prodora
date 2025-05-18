a8e6a607cfc691dc339597ee0dc7fc7d4814503c
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2025-05-18 22:20:38 +0300

    actually the shopController part is finished Error CS0029 will be fixed tomorrow

--- Prodora.WebUI/Controllers/ShopController.cs
+++ Prodora.WebUI/Controllers/ShopController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Prodora.Business.Abstract;
+using Prodora.Entitys;
+using Prodora.WebUI.Models;
 
 namespace Prodora.WebUI.Controllers
 {
@@ -15,7 +17,39 @@ namespace Prodora.WebUI.Controllers
 		[Route("products/{category?}")]
 		public IActionResult List(string categoryName , int page = 1)
 		{
-			return View();
+			const int pageSize = 5;
+
+			var products = new ProductListModel()
+			{
+				PageInfo = new PageInfo()
+				{
+					TotalItems = _productServices.GetCountByDivision(categoryName),
+					ItemsPerPage = pageSize,
+					CurrentCategory = categoryName,
+					CurrentPage = page
+				},
+				Products = _productServices.GetEProductByDivision(categoryName, page, pageSize)
+			};
+
+			return View(products);
+		}
+		
+		public IActionResult Details(int? id)	
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+			Product product = _productServices.GetEProductDetail(id.Value);
+			if (product == null)
+			{
+				return NotFound();
+			}
+			return View(new ProductDetail()
+			{
+				Products = product,
+				Categories = product.ProductCategory.Select(i => i.Category).ToList(),
+			});
 		}
 	}
 }
