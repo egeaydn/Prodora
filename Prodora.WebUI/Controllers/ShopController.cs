31d6527c9ac61c06a8397671f6aadc3dc7b82a4a
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2025-05-18 18:57:50 +0300

    new controller = ShopController

---
+++ Prodora.WebUI/Controllers/ShopController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prodora.Business.Abstract;
+
+namespace Prodora.WebUI.Controllers
+{
+	public class ShopController : Controller
+	{
+		IProductServices _productServices;
+
+		public ShopController( IProductServices productServices)
+		{
+			_productServices = productServices;
+		}
+
+		[Route("products/{category?}")]
+		public IActionResult List(string categoryName , int page = 1)
+		{
+			return View();
+		}
+	}
+}
