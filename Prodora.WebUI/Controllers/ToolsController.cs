d3decda6b2e7fcd724e28e8573b1d48061fcecd1
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2025-05-31 16:04:30 +0300

    ToolsController and Tools Pages added

---
+++ Prodora.WebUI/Controllers/ToolsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Prodora.WebUI.Controllers
+{
+	public class ToolsController : Controller
+	{
+		public IActionResult About()
+		{
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			return View();
+		}
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+		public IActionResult Terms()
+		{
+			return View();
+		}
+		public IActionResult Faq()
+		{
+			return View();
+		}
+		public IActionResult Help()
+		{
+			return View();
+		}
+		public IActionResult Sitemap()
+		{
+			return View();
+		}
+		public IActionResult Support()
+		{
+			return View();
+		}
+		public IActionResult Legal()
+		{
+			return View();
+		}
+		public IActionResult Cookies()
+		{
+			return View();
+		}
+		public IActionResult Accessibility()
+		{
+			return View();
+		}
+	}
+}
